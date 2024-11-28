5215f24a93f508493a137c6e2a2a518e44aae2b9
Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
2024-11-28 11:24:48 +0200

    Update

--- src/App/DTO/ProviderType.php
+++ src/App/DTO/ProviderType.php
@@ -13,4 +13,31 @@ enum ProviderType: string
     case GOOGLE = 'google';
     case VISICOM = 'visicom';
     case BY_AIR = 'by_air';
+
+    public static function getGeocodeProviderTypes(): array
+    {
+        return [
+            self::HERE,
+            self::GOOGLE,
+            self::VISICOM,
+        ];
+    }
+
+    public static function getDistanceProviderTypes(): array
+    {
+        return [
+            self::HERE,
+            self::GOOGLE,
+            self::VISICOM,
+            self::BY_AIR,
+        ];
+    }
+
+    public static function getAutocompleteProviderTypes(): array
+    {
+        return [
+            self::HERE,
+            self::GOOGLE,
+        ];
+    }
 }
